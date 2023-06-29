9021a234669dc0916f31c2126bf7a58f529e99ff
Victor Peralta <peralta.r.victor@gmail.com>
2023-06-28 19:56:43 -0700

    Add celery, beat and SQS to docker compose

--- starter/settings.py
+++ starter/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,3 +126,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Celery 
+CELERY_BROKER_TRANSPORT=os.environ.get("CELERY_BROKER_TRANSPORT")
+CELERY_BROKER_URL=os.environ.get("CELERY_BROKER_URL")
